df5ca9f57c60ae3d9389535a6f4093a1d0cbffc6
NavpreetST <999navtoor@gmail.com>
2026-06-07 19:25:05 +0200

    ci: fix test lint + datetime.UTC -> timezone.utc

--- tests/test_neurobus_contract.py
+++ tests/test_neurobus_contract.py
@@ -1,10 +1,17 @@
 from aegis.nexus.neurobus import NeuroState
 
+
+EXPECTED_VEC_LEN = 6
+EXPECTED_REWARD_CLAMPED = 1.0
+EXPECTED_THREAT_CLAMPED = -1.0
+
+
 def test_neurostate_vec_length():
-    assert len(NeuroState().vec()) == 6
+    assert len(NeuroState().vec()) == EXPECTED_VEC_LEN
+
 
 def test_neurostate_clamp_bounds():
     s = NeuroState(reward=9.0, threat=-9.0)
     s.clamp()
-    assert s.reward == 1.0
-    assert s.threat == -1.0
+    assert s.reward == EXPECTED_REWARD_CLAMPED
+    assert s.threat == EXPECTED_THREAT_CLAMPED
