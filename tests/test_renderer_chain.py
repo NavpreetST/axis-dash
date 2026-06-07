285b4a325ce0eeab460d27a686c84e4983a77af8
NavpreetST <999navtoor@gmail.com>
2026-06-07 19:17:05 +0200

    ci: fix ruff exclude globs + fix test lint errors

--- tests/test_renderer_chain.py
+++ tests/test_renderer_chain.py
@@ -1,4 +1,9 @@
-from aegis.renderer import RendererError, QuotaExhausted, TransientError
+from aegis.renderer import (
+    QuotaExhausted,
+    RendererError,
+    TransientError,
+)
+
 
 def test_exception_hierarchy():
     assert issubclass(QuotaExhausted, RendererError)
